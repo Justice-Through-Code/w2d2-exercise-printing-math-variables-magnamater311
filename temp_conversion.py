82c8461e8bd7c28353bb5b54cf6f9a6c2da290c6
Philosophers Fuel <donsells3535@gmail.com>
2023-09-07 13:19:19 -0700

    hotter

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,5 +1,5 @@
 
-#def convert_100_to_celsius():
+# def convert_100_to_celsius():
     # Convert a temperature of 100 degrees fahrenheit to celsius
     # Save this to a variable called celsius_100, and use print() to print out the value
 celsius_100 = 100
@@ -20,7 +20,7 @@ print ((celsius_100 - 32) * 5/9 )
 
 #convert_0_to_celsius()
 
-#def convert_34_2_to_celsius():
+# def convert_34_2_to_celsius():
     # Convert a temperature of 34.2 degrees fahrenheit to celsius
     # Do this one all in one print statement without saving any variables
 print ((32.2 - 32) * 5/9)
@@ -32,8 +32,9 @@ print ((32.2 - 32) * 5/9)
 print ((0.11 + 32) * 5/9)
 
 
-#def convert_5_to_fahrenheit():
+# def convert_5_to_fahrenheit():
     # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
+print ((5 - 32) * 5/9)
 
 #convert_5_to_fahrenheit()
 
@@ -41,4 +42,15 @@ print ((0.11 + 32) * 5/9)
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
 
+
 #hotter_temp()
+def hotter_temp():
+    celsius_temp = 30.2
+    fahrenheit_temp = 85.1
+    
+    if celsius_temp > (fahrenheit_temp - 32) * 5/9:
+        print(f"{celsius_temp} degrees celsius")
+    else:
+        print(f"{fahrenheit_temp} degrees fahrenheit")
+
+hotter_temp()
